3c5f00ecac1609e056828f8e4c3b757b584dff22
gawaine6 <50446718+gawaine6@users.noreply.github.com>
2021-03-15 14:49:31 +0900

    gradle, properties add oracle

--- src/main/java/com/testcode/yjp/last/domain/Member.java
+++ src/main/java/com/testcode/yjp/last/domain/Member.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class Member extends BaseEntity{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "member_id")
     private Long id;
 
