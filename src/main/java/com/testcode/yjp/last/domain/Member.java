1bab0380794573f243caf97075c5b21a8c96ad79
gawaine6 <50446718+gawaine6@users.noreply.github.com>
2021-03-18 14:55:31 +0900

    member_GenerationType_edit

--- src/main/java/com/testcode/yjp/last/domain/Member.java
+++ src/main/java/com/testcode/yjp/last/domain/Member.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 public class Member extends BaseEntity{
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "member_id")
     private Long id;
     private String user_id;
