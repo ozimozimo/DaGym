cc1565f29805ca50d10bcee15aeb722b83d304a7
pt7993@gmail.com <68317813+pt7993@users.noreply.github.com>
2021-05-01 12:07:58 +0900

    0501 멤버엔티티 update 추가

--- src/main/java/com/testcode/yjp/last/domain/Member.java
+++ src/main/java/com/testcode/yjp/last/domain/Member.java
@@ -75,13 +75,14 @@ public class Member extends BaseEntity{
         this.user_role = user_role;
     }
 
-    public void update(String user_pw, String user_name, String user_email, String address_normal, String address_detail, String user_role) {
+    public void update(String user_pw, String user_name, String user_email, String address_normal, String address_detail, String user_role, String user_rrn) {
         this.user_pw = user_pw;
         this.user_name = user_name;
         this.user_email = user_email;
         this.address_normal = address_normal;
         this.address_detail = address_detail;
         this.user_role = user_role;
+        this.user_rrn = user_rrn;
     }
 
 
