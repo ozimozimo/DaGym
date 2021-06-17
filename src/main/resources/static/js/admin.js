50753a67f144afa6ddc9441e93beee32c867e760
pt7993@gmail.com <68317813+pt7993@users.noreply.github.com>
2021-06-17 20:45:00 +0900

    06/17 관리자 메인 부트스트랩 및 html - 동기 회원 및 트레이너 값 컨트롤러 -> 뷰로 전달 - 지아, 창용 AdminApiController 삭제 - 지아 회원, 트레이너 html, 타임리프, js 작성 - 창용

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -4,19 +4,19 @@ function Delete() {
 
     var id = $(this).closest("tr").find("#member_id").text();
     console.log(id);
-    // console.log("id=" + id);
-    // $.ajax({
-    //     type: 'post',
-    //     url: '/recomments/delete/' + id,
-    //     dataType: 'json',
-    //     contentType: 'application/json; charset=utf-8'
-    // }).done(function () {
-    //     alert('답글이 삭제되었습니다.')
-    //     location.reload();
-    //     // window.location.href = "/";
-    // }).fail(function (error) {
-    //     alert(JSON.stringify(error));
-    // });
+
+    $.ajax({
+        type: 'delete',
+        url: '/admin/delete/' + id,
+        // dataType: 'json',
+        contentType: 'application/json; charset=utf-8'
+    }).done(function () {
+        alert('답글이 삭제되었습니다.')
+        location.reload();
+        // window.location.href = "/";
+    }).fail(function (error) {
+        alert(JSON.stringify(error));
+    });
 }
 
 $(function () {
