f154c16a95ffdaa83182e2a90faba598687c33ac
pt7993@gmail.com <68317813+pt7993@users.noreply.github.com>
2021-06-17 19:31:24 +0900

    0617 관리자페이지 메인 css적용 완료 회원, 트레이너 관리 페이지 값 띄우기 완료 삭제 진행중

--- src/main/java/com/testcode/yjp/last/controller/AdminController.java
+++ src/main/java/com/testcode/yjp/last/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.testcode.yjp.last.controller;
 
 import com.sun.org.apache.xpath.internal.operations.Mod;
 import com.testcode.yjp.last.domain.Member;
+import com.testcode.yjp.last.domain.dto.MemberList;
+import com.testcode.yjp.last.repository.MemberRepository;
 import com.testcode.yjp.last.service.AdminService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -10,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -26,18 +29,21 @@ public class AdminController {
 
     @GetMapping("/userManagement")
     public String userManagement(Model model) {
-        List<Member> members = adminService.selectUser();
-        model.addAttribute("members", members);
-        model.addAttribute("count", members.size());
+        List<MemberList> memberLists = adminService.selectUser();
+        log.info("memberLists = " + memberLists);
+        model.addAttribute("members", memberLists);
+        model.addAttribute("count", memberLists.size());
+        model.addAttribute("tr_if", "user");
         return "admin/userManagement";
     }
 
     @GetMapping("/trainerManagement")
     public String trainerManagement(Model model) {
-        List<Member> members = adminService.selectTrainer();
-        model.addAttribute("trainers", members);
-        model.addAttribute("count", members.size());
-        return "admin/trainerManagement";
+        List<MemberList> memberLists = adminService.selectTrainer();
+        model.addAttribute("members", memberLists);
+        model.addAttribute("count", memberLists.size());
+        model.addAttribute("tr_if", "trainer");
+        return "admin/userManagement";
     }
 
 
--- src/main/java/com/testcode/yjp/last/service/AdminService.java
+++ src/main/java/com/testcode/yjp/last/service/AdminService.java
@@ -1,11 +1,13 @@
 package com.testcode.yjp.last.service;
 
 import com.testcode.yjp.last.domain.Member;
+import com.testcode.yjp.last.domain.dto.MemberList;
 import com.testcode.yjp.last.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,11 +16,41 @@ import java.util.List;
 public class AdminService {
     private final MemberRepository memberRepository;
 
-    public List<Member> selectUser() {
-        return memberRepository.selectUser();
+    public List<MemberList> selectUser() {
+
+        List<Member> members = memberRepository.selectUser();
+        List<MemberList> memberLists = new ArrayList<>();
+        for(Member member : members) {
+            MemberList memberList = new MemberList();
+            memberList.setId(member.getId());
+            memberList.setUser_name(member.getUser_name());
+            memberList.setUser_id(member.getUser_id());
+            memberList.setUser_pn(member.getUser_pn());
+            memberList.setUser_rrn(member.getUser_rrn());
+            memberList.setAddress_normal(member.getAddress_normal());
+            memberList.setAddress_detail(member.getAddress_detail());
+            memberList.setRegDate(member.getRegDate());
+
+            memberLists.add(memberList);
+        }
+        return memberLists;
     }
 
-    public List<Member> selectTrainer() {
-        return memberRepository.selectTrainer();
+    public List<MemberList> selectTrainer() {
+        List<Member> members = memberRepository.selectTrainer();
+        List<MemberList> memberLists = new ArrayList<>();
+        for(Member member : members) {
+            MemberList memberList = new MemberList();
+            memberList.setId(member.getId());
+            memberList.setUser_name(member.getUser_name());
+            memberList.setUser_id(member.getUser_id());
+            memberList.setUser_pn(member.getUser_pn());
+            memberList.setUser_rrn(member.getUser_rrn());
+            memberList.setAddress_normal(member.getAddress_normal());
+            memberList.setAddress_detail(member.getAddress_detail());
+            memberList.setRegDate(member.getRegDate());
+            memberLists.add(memberList);
+        }
+        return memberLists;
     }
 }
