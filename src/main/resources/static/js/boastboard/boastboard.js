a64e9b56f9b84d10d56cfcd62eccb2a5bc159a68
JiALee <ljahpoao@naver.com>
2021-07-08 11:33:10 +0900

    0708 boastboard 수정

--- src/main/resources/static/js/boastboard/boastboard.js
+++ src/main/resources/static/js/boastboard/boastboard.js
@@ -126,28 +126,9 @@ function EmoInsert(emo) {
     console.log("reload");
 }
 
-function EmoSelect() {
-    let bb_num = $('#bb_num').val();
-    $.ajax({
-        type: 'GET',
-        url: '/boastboard/emotion/select/' + bb_num,
-        dataType: 'json',
-        contentType: 'application/json; charset=utf-8',
-    }).done(function(result) {
-        $('#l_num').text(result.l);
-        $('#m_num').text(result.m);
-        $('#s_num').text(result.s);
-        $('#a_num').text(result.a);
-        $('#w_num').text(result.w);
-    }).fail(function(fail) {
-
-    })
-}
-
 $(function (){
     $("#btn-save").on('click',Save);
     $("#btn-update").on('click',Update);
     $("#btn-delete").on('click',Delete);
-    EmoSelect();
 })
 
