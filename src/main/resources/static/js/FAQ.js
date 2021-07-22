6459586c707545b5b40a39ad7a96c80f5fa0a550
TAEK <dfcz1314@naver.com>
2021-07-22 14:27:55 +0900

    07.22 디자인 수정중10

--- src/main/resources/static/js/FAQ.js
+++ src/main/resources/static/js/FAQ.js
@@ -2,6 +2,15 @@
 $(function () {
     $('.q').on("click",function (){
         $(this).siblings('.a').toggle();
+
+        if($(this).find('i').hasClass('fa-chevron-down')){
+            $(this).find('i').removeClass('fa-chevron-down');
+            $(this).find('i').addClass('fa-chevron-up')
+        } else {
+            $(this).find('i').addClass('fa-chevron-down');
+            $(this).find('i').removeClass('fa-chevron-up')
+        }
+
         console.log(this);
 
     })
