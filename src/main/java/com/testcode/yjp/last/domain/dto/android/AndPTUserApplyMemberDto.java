6b2e5d52eaf826a381779d1ec8ab20c3f6ee3d97
pt7993@gmail.com <68317813+pt7993@users.noreply.github.com>
2021-07-22 10:43:03 +0900

    0722 안드

--- src/main/java/com/testcode/yjp/last/domain/dto/android/AndPTUserApplyMemberDto.java
+++ src/main/java/com/testcode/yjp/last/domain/dto/android/AndPTUserApplyMemberDto.java
@@ -1,5 +1,6 @@
 package com.testcode.yjp.last.domain.dto.android;
 
+import com.testcode.yjp.last.domain.Member;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,19 +9,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class AndPTUserApplyMemberDto {
-    private Long id;
     private String user_name;
     private String user_id;
-    private String start_date;
-    private String end_date;
 
-    public AndPTUserApplyMemberDto(Long id, String user_name, String user_id, String start_date, String end_date) {
-        this.id = id;
-        this.user_name = user_name;
-        this.user_id = user_id;
-        this.start_date = start_date;
-        this.end_date = end_date;
+    public AndPTUserApplyMemberDto(Member member) {
+        this.user_name = member.getUser_name();
+        this.user_id = member.getUser_id();
     }
-
-
 }
